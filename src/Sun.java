e7f80ad6f9ff9445e72609f1ffcf25c76264090e
Bryant Juarez <156694695+BryantBB@users.noreply.github.com>
2024-04-15 10:31:34 -0500

    Added Window Graphics

--- src/Sun.java
+++ src/Sun.java
@@ -2,15 +2,28 @@ import edu.macalester.graphics.*;
 
 public class Sun {
     
-    private Image sunSprite;
     private final int sunValue = 25;
+    private final int GRID_SIZE = 32;
+
+    private CanvasWindow canvas;
+    private GraphicsGroup sunSprite;
+
 
     public Sun() {
         loadSprite();
     }
 
     private void loadSprite(){
-        sunSprite = new Image("res\\game\\SUN.png");
+        Image sun = new Image("res\\game\\SUN.png");
+        sun.setMaxHeight(GRID_SIZE);
+        sun.setMaxWidth(GRID_SIZE);
+        sunSprite = new GraphicsGroup();
+        sunSprite.add(sun);
+        canvas.add(sunSprite);
+    }
+
+    public void removeSun(){
+        canvas.remove(sunSprite);
     }
     
 }
